bdf173e1f8c474302d4b69e790529db234a900fd
david.van.steertegem@gmail.com <PowerUser420>
2022-04-28 18:33:49 +0200

    added Streekbier class

---
+++ src/be/ehb/Streekbier.java
@@ -0,0 +1,16 @@
+package be.ehb;
+
+public class Streekbier {
+    private String naam;
+
+    public Streekbier() {
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+}
